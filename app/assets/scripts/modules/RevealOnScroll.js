17d22a43d337f5e8920ed4e6b3941df9f0c36acc
Shallman <shallman_q@hotmail.com>
2020-05-02 16:29:33 +0100

    Completed the reveal on scroll behavior.

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,54 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class RevealOnScroll {
+    constructor(els, thresholdPercent) {
+        this.thresholdPercent = thresholdPercent
+        this.itemsToReveal = els
+        this.browerHeight = window.innerHeight
+        this.hideInitially()
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+        this.events()
+    }
+
+    events() {
+        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener("resize", debounce( () => {
+            console.log("Resize just ran")
+            this.browerHeight = window.innerHeight
+        }, 333))
+    }
+
+    calcCaller() {
+        console.log("Scroll function ran")
+        this.itemsToReveal.forEach(el => {
+            if (el.isRevealed == false) {
+                this.calculateIfScrolledTo(el)
+            }
+        })
+    }
+
+    calculateIfScrolledTo(el) {
+        if (window.scrollY + this.browerHeight > el.offsetTop) {
+            console.log("Element was calculated")
+            let scrollPercent = (el.getBoundingClientRect().y / this.browerHeight) * 100
+            if (scrollPercent < this.thresholdPercent) {
+                el.classList.add("reveal-item--is-visible")
+                el.isRevealed = true
+                if (el.isLastItem) {
+                    window.removeEventListener("scroll", this.scrollThrottle)
+                }
+        }
+        }
+    }
+
+    hideInitially() {
+        this.itemsToReveal.forEach(el => {
+            el.classList.add("reveal-item")
+            el.isRevealed = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
+    }
+}
+
+export default RevealOnScroll;
